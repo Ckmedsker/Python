f4ecd68f9a7c117ff9c23ba3273f7a2701d25069
Ckmedsker <cameron.medsker@gmail.com>
2022-04-25 10:13:59 -0400

    Modified file structure

--- Debugging/M02_Code_Smell.py
+++
@@ -1,6 +0,0 @@
-uncleSam = 1.08
-paytheserver = 1.15
-foodstuff = input("How much was your meal? ")
-corporate_cut = uncleSam * int(foodstuff)
-pocket_book_pain = corporate_cut * paytheserver
-print("You need to leave " + str(pocket_book_pain) + " on the table to cover tax and tip.")
