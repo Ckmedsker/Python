a29e23081621b57848962febeb3cb721eea18bf5
Ckmedsker <cameron.medsker@gmail.com>
2022-12-16 12:57:01 -0500

    Added SongCreator from another repository.

--- PersonalProjects/TTS.py
+++ PersonalProjects/TTS.py
@@ -1,4 +1,5 @@
 import pyttsx3
 engine = pyttsx3.init()
-engine.say("Since IT students will be working in the Unix / Linux command line environment, it is useful to learn a least two common command-line text editors.")
+user_input = input("Enter in the text to be spoken to you!: ")
+engine.say(user_input)
 engine.runAndWait()
